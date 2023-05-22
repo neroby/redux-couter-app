61e12f1f8b38968c2b8e4f49db435e42eb4ab831
nermen_muhamed <narnarelaraby@gmail.com>
2023-05-23 01:01:40 +0200

    scope-provider

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import store from "./store/index";
-import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 // here we use <provider> to subscribe "connect" with the store to listen & update.
